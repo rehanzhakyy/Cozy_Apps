53ad702f8e3629ce6e49a76e313e4906c262fa57
raelhan <hannn9491@gmail.com>
2023-12-24 16:06:12 +0700

    update splash screen

---
+++ cozy_apps/lib/pages/splash_page.dart
@@ -0,0 +1,79 @@
+import 'package:cozy_apps/theme.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        bottom: false,
+        child: Stack(
+          children: [
+            Align(alignment: Alignment.bottomCenter, 
+            child: Image.asset('assets/images/splash_image.png')),
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 50,
+                left: 30,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 50,
+                    width: 50,
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('assets/images/logo.png'),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Text(
+                    'Find Cozy House\nto Stay and Happy',
+                    style: purpleTextStyle.copyWith(
+                      fontSize: 24,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Text(
+                    'Stop membuang banyak waktu\npada tempat yang tidak habitable',
+                    style: greyTextStyle.copyWith(
+                      fontSize: 16,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  Container(
+                    width: 210,
+                    height: 58,
+                    child: ElevatedButton(
+                      style: ButtonStyle(backgroundColor: MaterialStateProperty.all(purpleColor)), 
+                      onPressed: () {},
+                      // shape: RoundedRectangleBorder(
+                      //   borderRadius: BorderRadius.circular(17),
+                      // ),
+                      child: Text(
+                        'Explore Now',
+                        style: whiteTextStyle.copyWith(
+                          fontSize: 18,
+                        ),
+                        ),
+                      ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
